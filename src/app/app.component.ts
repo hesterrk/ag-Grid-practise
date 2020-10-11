b9521d1376ce86c5d648323016ddb8cfd34ff3ee
Hester Knight <hester2knight@mac.com>
2020-10-11 16:48:11 +0100

    injected http client dependency into app component and used dependency to get rowdata from server

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+
+// ngOnIniti => called by Angular to indicate that its done creating this component
+// We use it like this as its good practise if we use the ngOnInit() method
+export class AppComponent implements OnInit {
   title = 'my-grid-app';
 
   // Configuring properties of the grid
@@ -16,18 +20,32 @@ export class AppComponent {
   // Want to enable sorting based on price of the car: have to set the sortable property in each column we want to be able to sort by
   // -> sort the grid by clicking on col headers
 
-  // Want to enable filtering using the filter property 
-  // -> the grid will display a small column menu icon when you hover the header
-  
+  // Want to enable filtering using the filter property
+  // -> the grid will display a small column menu icon when you hover the header -> pressing displays a popup with a filtering UI which lets you choose the kind of filter and the text that you want to filter by
+
   columnDefs = [
     { field: 'make', sortable: true, filter: true },
     { field: 'model', sortable: true, filter: true },
     { field: 'price', sortable: true, filter: true },
   ];
 
-  rowData = [
-    { make: 'Toyota', model: 'Celica', price: 35000 },
-    { make: 'Ford', model: 'Mondeo', price: 32000 },
-    { make: 'Porsche', model: 'Boxter', price: 72000 },
-  ];
+  // Hard-coded data:
+  // rowData = [
+  //   { make: 'Toyota', model: 'Celica', price: 35000 },
+  //   { make: 'Ford', model: 'Mondeo', price: 32000 },
+  //   { make: 'Porsche', model: 'Boxter', price: 72000 },
+  // ];
+
+  // We dont know the type yet of the data from server
+  rowData: any;
+
+  // constructor is called before the ngOnInit()
+  // good for setting up dependency injection
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.rowData = this.http.get(
+      'https://raw.githubusercontent.com/ag-grid/ag-grid/master/grid-packages/ag-grid-docs/src/sample-data/smallRowData.json'
+    );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
 import { AgGridModule } from 'ag-grid-angular';
+import { HttpClientModule } from '@angular/common/http';
+
 
 // withComponents call is necessary for the grid to be able to use Angular components as cells / headers
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AgGridModule.withComponents([])],
+  imports: [BrowserModule, HttpClientModule, AgGridModule.withComponents([])],
   providers: [],
   bootstrap: [AppComponent],
 })
