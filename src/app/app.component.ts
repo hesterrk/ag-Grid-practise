bee32a8072f412954df4997b3d6c8376183a0a41
Hester Knight <hester2knight@mac.com>
2020-10-11 12:20:22 +0100

    ag-grid set up with project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'my-grid-app';
+
+  // Configuring properties of the grid
+  // -> column definitions
+  // -> each column entry has header label + data field
+  // -> the data (rowData)
+
+  columnDefs = [{ field: 'make' }, { field: 'model' }, { field: 'price' }];
+
+  rowData = [
+    { make: 'Toyota', model: 'Celica', price: 35000 },
+    { make: 'Ford', model: 'Mondeo', price: 32000 },
+    { make: 'Porsche', model: 'Boxter', price: 72000 },
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { AgGridModule } from 'ag-grid-angular';
+
+// withComponents call is necessary for the grid to be able to use Angular components as cells / headers
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AgGridModule.withComponents([])],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
